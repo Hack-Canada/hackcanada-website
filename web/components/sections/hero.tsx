b43cc0d5dd1139b4dffa11fb4183c3fb234b0c3b
faizm10 <faizmustansar10@gmail.com>
2026-01-17 15:10:53 -0500

    Change button text from 'Apply Now!' to 'Coming Soon'

--- web/components/sections/hero.tsx
+++ web/components/sections/hero.tsx
@@ -96,7 +96,7 @@ export default function Hero() {
         <p className='text-[#441E0A] text lg:text-2xl mt-2 lg:mt-0'>Feb 30-32, 2025 | In-Person Event | Unofficial MLH Partner</p>
         <div className='mt-4 lg:mt-6 flex flex-col items-center'>
           <div className='bg-[#441E0A] text-white px-8 lg:px-10 py-2 lg:py-3 rounded-t-lg lg:rounded-lg hover:bg-[#5C2E0F] transition'>
-            <a href="" className='font-bold text-lg lg:text-2xl'>Apply Now!</a>
+            <a href="" className='font-bold text-lg lg:text-2xl'>Coming Soon</a>
           </div>
           <div className='bg-[#EC294D] text-white px-4 py-1 rounded-b-lg text-center'>
             <span className='font-semibold text-sm'>Due Feb 20, 2025!</span>
