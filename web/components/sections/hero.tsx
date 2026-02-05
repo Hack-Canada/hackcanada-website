a0c81e7a1e8877ca9ce57b6d2437ab6d5297eabc
James Cao <jameslodgemath@gmail.com>
2026-02-05 18:44:00 -0500

    changed to 2026 on hero page

--- web/components/sections/hero.tsx
+++ web/components/sections/hero.tsx
@@ -87,7 +87,7 @@ export default function Hero() {
           Hack Canada
         </h1>
         <p className="text-[#441E0A] text lg:text-2xl mt-2 lg:mt-0 font-rubik">
-          March 6-8, 2025 | In-Person Event
+          March 6-8, 2026 | In-Person Event
         </p>
         <div className="mt-4 lg:mt-6 flex flex-col items-center">
           <div className="bg-[#441E0A] text-white px-8 lg:px-10 py-2 lg:py-3 rounded-t-lg lg:rounded-lg hover:bg-[#5C2E0F] transition">
